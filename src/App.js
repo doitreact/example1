6c29f45419e06ec8ba1968624c2c0717e39235d3
두잇 리액트 실습 데모 <onlytoyou@gmail.com>
2022-12-27 00:15:20 +0000

    예제 01-3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './styles.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>두잇 리액트 데모</h1>
+      <h1 className="title">두잇 리액트 데모</h1>
     </div>
   );
 }
